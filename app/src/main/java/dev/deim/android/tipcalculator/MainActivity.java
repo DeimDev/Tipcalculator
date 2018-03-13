756e95f2dc4ddc232938e72a544759b1343544ab
DeimDev <develeved@gmail.com>
2018-03-14 00:00:15 +0300

    version 1.0

--- app/src/main/java/dev/deim/android/tipcalculator/MainActivity.java
+++ app/src/main/java/dev/deim/android/tipcalculator/MainActivity.java
@@ -1,13 +1,91 @@
 package dev.deim.android.tipcalculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormat = NumberFormat.getPercentInstance();
+    private double billAmount = 0.0;
+    private double percent = 0.15;
+    private TextView amountTextView;
+    private TextView percentTextView;
+    private TextView tipTextView;
+    private TextView totalTextView;
+    private final SeekBar.OnSeekBarChangeListener seekBarListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress,
+                                      boolean fromUser) {
+            percent = progress / 100.0;
+            calculate();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+        }
+    };
+    private final TextWatcher amountEditTextWatcher = new TextWatcher() {
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            try {
+                billAmount = Double.parseDouble(s.toString()) / 100.0;
+                amountTextView.setText(currencyFormat.format(billAmount));
+            } catch (NumberFormatException e) {
+                amountTextView.setText("");
+                billAmount = 0.0;
+            }
+            calculate();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+
+        @Override
+        public void beforeTextChanged(
+                CharSequence s, int start, int count, int after) {
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        amountTextView = (TextView) findViewById(R.id.amount_TextView);
+        percentTextView = (TextView) findViewById(R.id.percent_TextView);
+        tipTextView = (TextView) findViewById(R.id.tip_TextView);
+        totalTextView = (TextView) findViewById(R.id.total_TextView);
+
+        tipTextView.setText(currencyFormat.format(0));
+        totalTextView.setText(currencyFormat.format(0));
+
+        EditText amountEditText = (EditText) findViewById(R.id.amount_EditText);
+        amountEditText.addTextChangedListener(amountEditTextWatcher);
+
+        SeekBar percentSeekBar = (SeekBar) findViewById(R.id.percent_SeecBar);
+        percentSeekBar.setOnSeekBarChangeListener(seekBarListener);
+
+
+    }
+
+    private void calculate() {
+        percentTextView.setText(percentFormat.format(percent));
+        double tip = billAmount * percent;
+        double total = billAmount + tip;
+        tipTextView.setText(currencyFormat.format(tip));
+        totalTextView.setText(currencyFormat.format(total));
     }
 }
